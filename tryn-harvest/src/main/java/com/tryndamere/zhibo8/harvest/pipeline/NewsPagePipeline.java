49010dd84bccb059f23699a14785f1a24f3836ad
Dave <389788728@qq.com>
2018-12-28 21:33:39 +0800

    新闻统计

--- tryn-harvest/src/main/java/com/tryndamere/zhibo8/harvest/pipeline/NewsPagePipeline.java
+++ tryn-harvest/src/main/java/com/tryndamere/zhibo8/harvest/pipeline/NewsPagePipeline.java
@@ -1,6 +1,7 @@
 package com.tryndamere.zhibo8.harvest.pipeline;
 
 import com.tryndamere.zhibo8.harvest.processor.NewsPageProcessor;
+import com.tryndamere.zhibo8.trynbusiness.business.NewsPageBusiness;
 import com.tryndamere.zhibo8.trynbusiness.business.impl.NewsPageBusinessImpl;
 import com.tryndamere.zhibo8.trynmodel.entity.NewsPage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +20,13 @@ import java.util.List;
 @Component
 public class NewsPagePipeline implements Pipeline {
     @Autowired
-    private NewsPageBusinessImpl newsPageBusiness;
+    private NewsPageBusiness newsPageBusiness;
 
     @Override
     public void process(ResultItems resultItems, Task task) {
         List<NewsPage> morePages=resultItems.get("more");
+        System.out.println("size"+morePages.size());
+
         newsPageBusiness.saveBatch(morePages);
 
     }
--- tryn-harvest/src/test/java/com/tryndamere/zhibo8/harvest/processor/NewsPageProcessorTest.java
+++ tryn-harvest/src/test/java/com/tryndamere/zhibo8/harvest/processor/NewsPageProcessorTest.java
@@ -1,7 +1,9 @@
 package com.tryndamere.zhibo8.harvest.processor;
 
+import com.tryndamere.zhibo8.harvest.pipeline.NewsPagePipeline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import us.codecraft.webmagic.Spider;
@@ -16,12 +18,13 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NewsPageProcessorTest {
-
+    @Autowired
+    private NewsPagePipeline newsPagePipeline;
 
     @Test
     public void testDig() throws Exception {
         Spider.create(new NewsPageProcessor())
-                .addPipeline(new ConsolePipeline())
+                .addPipeline(newsPagePipeline)
                 .addUrl("https://news.zhibo8.cc/nba/more.htm")
                 .run();
     }

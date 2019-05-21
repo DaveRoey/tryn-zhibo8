f6c125cc661d95f2dd42fc46cf317a95bf9e4767
daveroey <389788728@qq.com>
2019-05-21 16:01:20 +0800

    redis测试

--- tryn-api/src/main/java/com/tryndamere/zhibo8/trynapi/action/EmployeeAction.java
+++ tryn-api/src/main/java/com/tryndamere/zhibo8/trynapi/action/EmployeeAction.java
@@ -1,43 +1,39 @@
 package com.tryndamere.zhibo8.trynapi.action;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Dave on 2018/12/20
  * Describes
  */
 @RestController
+@Configuration
 @RequestMapping(value = "/api/*")
 public class EmployeeAction extends BaseAction {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
     @GetMapping("hello")
     public String hello() {
-        return "hello";
+         restTemplate.put("http://127.0.0.1:8082/api/put","a");
+        return "aa";
+    }
+    @PutMapping("put")
+    public String put(String a) {
+        return "ok";
     }
 
-
-    public static void main(String[] args) {
-        String a="processdata\n" +
-                "quest_sl_temp_explain1\n" +
-                "task\n" +
-                "temp_relation\n" +
-                "tlevel\n" +
-                "unit\n" +
-                "useroperation\n" +
-                "vendor\n" +
-                "wf_h_processinst\n" +
-                "wf_roletemp\n" +
-                "wfactivityinst\n" +
-                "wfauditlog\n" +
-                "wfprocessdefine\n" +
-                "wfprocessinst\n" +
-                "wftransctrl\n" +
-                "wftransition";
-
-        for (String s : a.split("\n")) {
-            System.out.println("drop table if EXISTS "+s+";");
-        }
-    }
 }
--- tryn-api/src/test/java/com/tryndamere/zhibo8/trynapi/TrynApiApplicationTests.java
+++ tryn-api/src/test/java/com/tryndamere/zhibo8/trynapi/TrynApiApplicationTests.java
@@ -1,17 +1,23 @@
 package com.tryndamere.zhibo8.trynapi;
 
+import com.tryndamere.zhibo8.redis.repostory.RedisManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TrynApiApplicationTests {
-
+	@Autowired
+	RedisManager redisManager;
 	@Test
 	public void contextLoads() {
+		redisManager.set("aa","aa");
+		System.out.println(redisManager.get("aa"));
 	}
+
 
 }
 
